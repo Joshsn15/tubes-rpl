1aeb33f240016b82fc57ff51b5d67997ed9ecbce
JenniferAverina <angelaa.ave@gmail.com>
2026-04-20 16:39:26 +0700

    stocker done

--- front-end/src/config/AppRoutes.tsx
+++ front-end/src/config/AppRoutes.tsx
@@ -4,14 +4,16 @@ import Layout from '../components/Layout.jsx';
 
 const FormLaporan = lazy(() => import('../pages/stockForm.tsx'));
 const ReceivalBarang = lazy(() => import('../pages/receivalBarang.tsx'));
+const StockPage = lazy(() => import('../pages/stockPage.jsx'));
 
 export const AppRoutes = () => {
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <Routes>
                 <Route element={<Layout />}>
-                    <Route path="/stock" element={<FormLaporan />} />
+                    <Route path="/form" element={<FormLaporan />} />
                     <Route path="/receival" element={<ReceivalBarang />} />
+                    <Route path="/stock" element={<StockPage />} />
                 </Route>
             </Routes>
         </Suspense>
